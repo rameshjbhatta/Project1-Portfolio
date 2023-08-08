7998d7b79b57c20e70b2e07286c7af114b9ab099
rameshjbhatta <rameshjbhatta@gmail.com>
2023-08-08 13:25:51 +0545

    modified template & created model mi

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from portfolio.models import *
 
 # Register your models here.
+class MessageAdminView(admin.ModelAdmin):
+    list_display = ['id','name','email','msg']
+
+admin.site.register(MessageInfo,MessageAdminView)
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MessageInfo(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=150)
+    msg = models.TextField(max_length=350)
